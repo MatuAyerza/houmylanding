a7de834f5e2032a09f30d7bce99fbe30d6a6dcdd
MatuAyerza <matu.ayerza@gmail.com>
2022-05-24 10:43:54 -0300

    Favicon and Team pictures

--- src/components/navigation.jsx
+++ src/components/navigation.jsx
@@ -16,7 +16,7 @@ export const Navigation = (props) => {
             <span className='icon-bar'></span>{" "}
           </button>
           <a className='navbar-brand page-scroll' href='#page-top'>
-            <img src='img/logos/logodeepblue.jpg' className='img-responsive' alt='' />{" "}
+            <img src='img/logos/logodeepbluenobg.jpg' className='img-responsive' alt='' />{" "}
             Houmy
           </a>{" "}
         </div>
